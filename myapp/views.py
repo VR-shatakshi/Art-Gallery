bed73a74f41d878583b555b650af260118247303
VR-Shatakshi <shatakshishrivastava480@gmail.com>
2025-11-23 16:40:25 +0530

    Refactor image handling in views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ImageForm
-
 from .models import Image
-# Create your views here.
+
 def home(request):
-    form = ImageForm()
-    
     if request.method == 'POST':
-     form = ImageForm(request.POST,request.FILES)
-     if form.is_valid():
-      form.save()
-    form = ImageForm()
-    img = Image.objects.all()
-    return render(request,'home.html',{'img':img,'form':form})
-  
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = ImageForm()
+    
+    img = Image.objects.all().order_by('-date')  
+    return render(request, 'home.html', {'img': img, 'form': form})
+
 def delete_img(request, id):
-    img = Image.objects.get(id=id)
-    img.delete()
+    img = get_object_or_404(Image, id=id)
+    img.delete()  
     return redirect('/')
 
 
