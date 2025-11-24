d48572bbf7804435b00e4077dde9f1d377e48cb5
VR-Shatakshi <shatakshishrivastava480@gmail.com>
2025-11-24 07:50:47 +0530

    Add permanent_url property to model
    Add a property to retrieve the permanent URL of the photo.

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,11 @@ class Image(models.Model):
     photo = CloudinaryField('image')
     date = models.DateTimeField(auto_now_add=True)
 
+   @property
+    def permanent_url(self):
+        url, options = cloudinary_url(self.photo.public_id, secure=True)
+        return url
+
     def __str__(self):
         return self.title or f"Image {self.id}"
 
